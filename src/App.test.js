990b79245037d7e8a755847d918124397ea722cb
Michael Dandini <mdandini@esri.com>
2023-02-27 19:23:11 -0500

    add jest test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,36 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock("./components/MapContainer", () => {
+  return () => {
+    return <div>Map Container</div>;
+  };
+});
+
+jest.mock("./components/BookmarkNav", () => {
+  return () => {
+    return <div>Bookmark Nav</div>;
+  };
+});
+
+jest.mock("./components/InfoLookup", () => {
+  return () => {
+    return <div>Info Lookup</div>;
+  };
+});
+
+jest.mock("@esri/calcite-components-react", () => {
+  return {
+    CalciteShell: ({ children }) => <div>{children}</div>,
+    CalciteShellPanel: ({ children }) => <div>{children}</div>,
+    CalcitePanel: ({ children }) => <div>{children}</div>,
+  };
+});
+
+test("renders learn react link", () => {
+  const { getByText } = render(<App />);
+
+  expect(getByText("Map Container")).toBeInTheDocument();
+  expect(getByText("Bookmark Nav")).toBeInTheDocument();
+  expect(getByText("Info Lookup")).toBeInTheDocument();
 });
