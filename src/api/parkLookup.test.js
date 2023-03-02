97c8a80c5514b3b16256db4eb5ca868e6f4f1b32
Michael A. Dandini <mdandini@esri.com>
2023-03-02 14:24:50 -0500

    parkLookup test

---
+++ src/api/parkLookup.test.js
@@ -0,0 +1,32 @@
+import { parkLookup } from "./parkLookup";
+
+beforeEach(() => {
+  fetchMock.doMock();
+});
+
+test("it calls the remote service with correct URL", (done) => {
+  // Mock out fetch
+  fetch.mockResponse(JSON.stringify({ info: "it's a park" }));
+
+  parkLookup("findThisPark").then((resp) => {
+    expect(resp).toBe("it's a park");
+    done();
+  });
+
+  expect(fetch.mock.calls[0][0]).toEqual("/park-lookup?park=findThisPark");
+});
+
+test("it throws errors with invalid responses", (done) => {
+  const theError = new Error("Expected Error");
+  fetch.mockReject(theError);
+
+  parkLookup("findThisPark")
+    .then(() => {
+      // We shouldn't see this. Fail test
+      fail("fetch not failed");
+    })
+    .catch((err) => {
+      expect(err).toBe(theError);
+      done();
+    });
+});
